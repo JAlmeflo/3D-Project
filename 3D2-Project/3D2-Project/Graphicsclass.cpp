a69686d4b09d8163696dc8f20e75a530e4ea886f
Flow <johan_almeflo@hotmail.com>
2014-10-05 21:55:17 +0200

    added graphicsclass

---
+++ 3D2-Project/3D2-Project/Graphicsclass.cpp
@@ -0,0 +1,30 @@
+#include "Graphicsclass.h"
+
+
+Graphicsclass::Graphicsclass()
+{
+}
+
+
+Graphicsclass::~Graphicsclass()
+{
+}
+
+bool Graphicsclass::Initialize(int, int, HWND)
+{
+	return true;
+}
+
+void Graphicsclass::Shutdown()
+{
+}
+
+bool Graphicsclass::Frame()
+{
+	return true;
+}
+
+bool Graphicsclass::Render()
+{
+	return true;
+}
