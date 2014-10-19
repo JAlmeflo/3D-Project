0d82dfe6ace9226c588b6fdc2f349782046e6c14
Flow <johan_almeflo@hotmail.com>
2014-10-19 18:41:13 +0200

    shadow map shaders

--- 3D2-Project/3D2-Project/LightPS.hlsl
+++ 3D2-Project/3D2-Project/LightPS.hlsl
@@ -3,15 +3,16 @@
 //	return float4(1.0f, 1.0f, 1.0f, 1.0f);
 //}
 
-Texture2D shaderTexture;
-SamplerState samplerType;
+Texture2D shaderTexture : register(t0);
+Texture2D depthMapTexture : register(t1);
+
+SamplerState samplerTypeClamp : register(s0);
+SamplerState samplerTypeWrap : register(s1);
 
 cbuffer LightBuffer
 {
 	float4 ambientColor;
 	float4 diffuseColor;
-	float3 lightDirection;
-	float padding;
 };
 
 struct PixelInputType
@@ -19,31 +20,52 @@ struct PixelInputType
 	float4 position : SV_POSITION;
 	float2 tex : TEXCOORD0;
 	float3 normal : NORMAL;
+	float4 lightViewPosition : TEXCOORD1;
+	float3 lightPos : TEXCOORD2;
 };
 
 // Pixel Shader
 float4 main(PixelInputType input) : SV_TARGET
 {
+	float bias;
 	float4 textureColor;
-	float3 lightDir;
 	float lightIntensity;
-	float4 color;	
+	float4 color;
+	float2 projectTexCoord;
+	float depthValue;
+	float lightDepthValue;
 
-	textureColor = shaderTexture.Sample(samplerType, input.tex);
-
-	lightDir = -lightDirection;
-
-	lightIntensity = saturate(dot(input.normal, lightDir));
+	bias = 0.001f;
 
 	color = ambientColor;
 
-	if (lightIntensity > 0.0f)
+	// Calc projected texture coordinates
+	projectTexCoord.x = input.lightViewPosition.x / input.lightViewPosition.w * 0.5f + 0.5f;
+	projectTexCoord.y = -input.lightViewPosition.y / input.lightViewPosition.w * 0.5f + 0.5f;
+
+	// if (0-1) then pixel is in the view of light
+	if ((saturate(projectTexCoord.x) == projectTexCoord.x) && (saturate(projectTexCoord.y) == projectTexCoord.y))
 	{
-		color += (diffuseColor * lightIntensity);
+		depthValue = depthMapTexture.Sample(samplerTypeClamp, projectTexCoord).r;
+
+		lightDepthValue = input.lightViewPosition.z / input.lightViewPosition.w;
+		lightDepthValue = lightDepthValue - bias;
+
+		// Light hit the pixel
+		if (lightDepthValue < depthValue)
+		{
+			lightIntensity = saturate(dot(input.normal, input.lightPos));
+
+			if (lightIntensity > 0.0f)
+			{
+				color += (diffuseColor * lightIntensity);
+				color = saturate(color);
+			}
+		}
 	}
 
-	color = saturate(color);
-	
+
+	textureColor = shaderTexture.Sample(samplerTypeWrap, input.tex);	
 	color = color * textureColor;
 
 	return color;
--- 3D2-Project/3D2-Project/LightVS.hlsl
+++ 3D2-Project/3D2-Project/LightVS.hlsl
@@ -8,7 +8,15 @@ cbuffer MatrixBuffer
 	matrix worldMatrix;
 	matrix viewMatrix;
 	matrix projectionMatrix;
+	matrix lightViewMatrix;
+	matrix lightProjectionMatrix;
 };
+
+cbuffer LightBuffer2
+{
+	float3 lightPosition;
+	float padding;
+}
 
 struct VertexInputType
 {
@@ -22,12 +30,15 @@ struct PixelInputType
 	float4 position : SV_POSITION;
 	float2 tex : TEXCOORD0;
 	float3 normal : NORMAL;
+	float4 lightViewPosition : TEXCOORD1;
+	float3 lightPos : TEXCOORD2;
 };
 
 // Vertex Shader
 PixelInputType main(VertexInputType input)
 {
 	PixelInputType output;
+	float4 worldPosition;
 
 	input.position.w = 1.0f;
 
@@ -35,10 +46,18 @@ PixelInputType main(VertexInputType input)
 	output.position = mul(output.position, viewMatrix);
 	output.position = mul(output.position, projectionMatrix);
 
+	output.lightViewPosition = mul(input.position, worldMatrix);
+	output.lightViewPosition = mul(output.lightViewPosition, lightViewMatrix);
+	output.lightViewPosition = mul(output.lightViewPosition, lightProjectionMatrix);
+
 	output.tex = input.tex;
 
 	output.normal = mul(input.normal, (float3x3)worldMatrix);
 	output.normal = normalize(output.normal);
+
+	worldPosition = mul(input.position, worldMatrix);
+	output.lightPos = lightPosition.xyz - worldPosition.xyz;
+	output.lightPos = normalize(output.lightPos);
 
 	return output;
 }
